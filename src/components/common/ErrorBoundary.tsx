3a439272cbe6199c53c171bcf31b88208fe1eac9
AugustoL <augustolemble@pm.me>
2025-12-11 08:52:13 -0300

    fix: fix index redirection on error page

--- src/components/common/ErrorBoundary.tsx
+++ src/components/common/ErrorBoundary.tsx
@@ -40,7 +40,8 @@ export class ErrorBoundary extends Component<Props, State> {
   };
 
   private handleGoHome = () => {
-    window.location.href = "/";
+    const isGhPages = typeof window !== "undefined" && window.location.hostname.includes("github.io");
+    window.location.href = isGhPages ? "/explorer/" : "/";
   };
 
   render() {
